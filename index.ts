45c7ee20f0e197ac3aed3e8e8602d6f31abcb979
longj724 <justinlong596@gmail.com>
2024-02-17 16:45:30 -0500

    Error handling middleware

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // External Dependencies
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 // Relative Dependencies
@@ -15,6 +15,18 @@ app.use(bodyParser.json());
 
 app.use('/meals', mealRouter);
 app.use('/insights', insightsRouter);
+
+app.use((error: any, _req: Request, res: Response, _next: NextFunction) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+  res.status(error.statusCode).json({
+    message: error.message,
+  });
+});
+
+app.use('*', (_req, res) => {
+  res.status(404).send('404 Not Found: The requested resource does not exist.');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
